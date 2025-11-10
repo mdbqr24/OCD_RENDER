9600541cd59f080bb8753dcaeb4e54749d59cb4e
22B6014 <22B6014@ubd.edu.bn>
2025-11-10 20:50:58 +0800

    yes

--- OCD/config/services.php
+++ OCD/config/services.php
@@ -27,7 +27,9 @@ return [
         'secret' => env('AWS_SECRET_ACCESS_KEY'),
         'region' => env('AWS_DEFAULT_REGION', 'us-east-1'),
     ],
-
+    'ai_detection' => [
+        'endpoint' => env('AI_DETECTION_ENDPOINT', 'http://127.0.0.1:5000/predict_keras_model'),
+    ],
     'slack' => [
         'notifications' => [
             'bot_user_oauth_token' => env('SLACK_BOT_USER_OAUTH_TOKEN'),
